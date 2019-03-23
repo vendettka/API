a2ad52c9adc8706de656273c881566804822025d
vendettka <45879724+vendettka@users.noreply.github.com>
2019-03-23 17:22:19 +0400

    Первая задача

---
+++ 2.py
@@ -0,0 +1,45 @@
+import pygame
+import requests
+import sys
+import os
+x,y,c=map(str, input().split())
+params = {
+        "ll": x+","+y,
+        "spn": c,
+        "l": "map"
+    }
+response = None
+try:
+    map_request = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_request, params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        print(response.content)
+
+        sys.exit(1)
+except:
+    print("Запрос не удалось выполнить. Проверьте наличие сети Интернет.")
+    sys.exit(1)
+
+# Запишем полученное изображение в файл.
+map_file = "map.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+# Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+os.remove(map_file)
